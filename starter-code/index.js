b74a1477c21ea4a5876467e06a78390471fee3cc
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-06-22 15:48:47 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
+'use strict';
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      return a - b;
+    })
+    this.length = this.items.length;
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
+  max() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return Math.max.apply(null, this.items)
+  }
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return Math.min.apply(null, this.items)
+  }
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((total, num) => total += num)
+  }
+  average(){
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList')
+    }
+    return this.sum() / this.length;
+  }
 };
 
 module.exports = SortedList;
